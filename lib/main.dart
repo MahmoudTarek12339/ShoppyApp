ebdcef0797d616d4f973112d8dd841717be9f5cf
mahmoudTarek12339@gmail.com <mahmoud12339@gmail.com>
2021-11-09 23:01:09 +0200

    mahmoud tarek

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:OnBoardingScreen(),
+      home:startWidget,
     );
   }
 }
