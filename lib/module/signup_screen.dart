5a7ba458e8919afb21ec6dfdf413d0c5510f228f
mahmoudTarek12339@gmail.com <mahmoud12339@gmail.com>
2021-11-10 19:30:46 +0200

    mahmoud tarek 10_11

---
+++ lib/module/signup_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('sign up'),
+      ),
+    );
+  }
+}
